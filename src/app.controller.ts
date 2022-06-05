48702cedaed13c21c864599458f39b61981c7da0
Juan <jcuello673@gmail.com>
2022-06-05 03:19:22 -0300

    deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('holaMundo')
+  @Get('user')
   getHello(): Object {
-    return this.appService.getHello();
+    return this.appService.getUser();
   }
 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+export enum Country {
+  PE = 'Peru',
+  CO = 'Colombia',
+  AR = 'Argentina'
+}
+
 @Injectable()
 export class AppService {
-  getHello(): Object {
-    const response = {
-      message : "Hola Mundo",
-      statusCode : 200,
+  getUser(): Object {
+    const user = {
+      username : 'juancuello98',
+      password : 'assfg99Agkzx54',
+      country: Country.AR ,
+      address: 'Asturias 3023, Cordoba, Cordoba',
+      origin: 'Cordoba',
+      destination: 'Buenos Aires'
     }
-    return response;
+    return user;
   }
 }
