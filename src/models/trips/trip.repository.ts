481b535db8dd3a452a04fc26bff27839c239cb4f
Juan Cuello <77285@sistemas.frc.utn.edu.ar>
2024-03-24 00:42:08 -0300

    feat: separo el findById por defecto del que devuelve los datos del driver

--- src/models/trips/trip.repository.ts
+++ src/models/trips/trip.repository.ts
@@ -1,6 +1,6 @@
 import { User, UserDocument } from '@/users/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { NewTripDTO } from './dto/new-trip.dto';
 import { TripStatus } from './enums/state.enum';
 import { Trip, TripDocument } from './trip.schema';
@@ -16,6 +16,7 @@ export class TripRepository {
     const trips = await this.tripModel
       .find({ driver })
       .sort({ createdTimestamp: 'desc' })
+      .select('-__v')
       .exec();
     return trips;
   }
@@ -24,13 +25,20 @@ export class TripRepository {
     return await this.tripModel.find(field).exec();
   }
 
-  async findById(id: any): Promise<TripDocument | any> {
-    const trip = this.tripModel.findById(id).select('-__v').populate('driver').exec();
+  async findByIdWithDriver(id: any): Promise<any> {
+    const trip = await this.tripModel.findById(id).select('-__v').lean().exec();
+    const driverFields = ['email','name','lastname'];
+    const driver = await this.userModel.findOne({email: trip.driver}).select(driverFields.join(' ')).exec();
+    return {...trip,driver};
+  }
+
+  async findById(id: string): Promise<any> {
+    const trip = await this.tripModel.findById(id).select('-__v').exec();
     return trip;
   }
 
   async findNonDriverTrips(email: string) {
-    return await this.tripModel.find({ driver: { $ne: email } });
+    return await this.tripModel.find({ driver: { $ne: email } }).select('-__v').exec();
   }
 
   async create(trip: NewTripDTO) {
@@ -52,27 +60,51 @@ export class TripRepository {
     return tripUpdated;
   }
 
-  async findByIdAndDriver(driver: string, _id: string): Promise<TripDocument> {
+  async updateStatus(tripId: string, newStatus: TripStatus) {
+    try {
+      // Buscar el viaje por su ID
+      const trip = await this.tripModel.findById(tripId);
+
+      if (!trip) {
+        throw new Error('Trip not found');
+      }
+
+      // Actualizar el campo status
+      trip.status = newStatus;
+
+      // Guardar los cambios en la base de datos
+      const updatedTrip = await trip.save();
+
+      return updatedTrip;
+    } catch (error) {
+      throw new Error(`Failed to update trip status: ${error.message}`);
+    }
+  }
+
+  async findByIdAndDriver(driver: string, _id: string): Promise<any> {
     const filter = {
       driver,
       _id,
     };
 
-    const hasUserTrip = await this.tripModel.findOne(filter);
+    const hasUserTrip = await this.tripModel.findOne(filter).lean().exec();
 
     return hasUserTrip;
   }
 
-  async findPassengers(
-    passengerIds: string[],
-    fieldsToSelect: string[],
-  ): Promise<any[]> {
+  async passengersByTrip(
+    id: string,
+  ): Promise<any> {
     try {
-      const passengers = await this.userModel
-        .find({ _id: { $in: passengerIds } })
-        .select(fieldsToSelect.join(' '));
+      const trip = this.tripModel
+      .findOne({id})
+      .populate({
+        path: 'passengers',
+        select: 'name lastname email -_id'}
+      )
+      .exec();
 
-      return passengers;
+      return trip;
     } catch (error) {
       throw new Error('Error al buscar pasajeros: ' + error.message);
     }
