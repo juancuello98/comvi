0dd6fe54dc72952cea5f457a3e89fe6a876656a0
Juan Cuello <77285@sistemas.frc.utn.edu.ar>
2024-07-24 23:52:31 -0300

    refactor: remove request module

--- src/models/valuations/valuations.module.ts
+++ src/models/valuations/valuations.module.ts
@@ -4,14 +4,14 @@ import { ValuationsController } from './valuations.controller';
 import { User, UserSchema } from '../users/user.schema';
 import { Trip, TripSchema } from '../trips/trip.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RequestSchema } from '../requests/request.schema';
+// import { RequestSchema } from '../requests/request.schema';
 import { CommonModule } from 'src/common/common.module';
 import { Valuation, ValuationSchema } from './entities/valuation.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Request.name, schema: RequestSchema },
+      // { name: Request.name, schema: RequestSchema },
       { name: Trip.name, schema: TripSchema },
       { name: User.name, schema: UserSchema },
       { name: Valuation.name, schema: ValuationSchema },
