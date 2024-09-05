95ed05eaddb5d05c475f6cf42e816e950f71184b
Juan Cuello <juancuello@Air-Juan.fibertel.com.ar>
2024-09-04 22:49:57 -0300

    feat: add healthy endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+ @ApiTags('health')
+  @Post('health')
+  health(): string {
+    return this.appService.healthy();
   }
 }
