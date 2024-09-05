e7bc6d8bfc624f6d7ef4a6f3597c2affbbae93b1
Juan Cuello <juancuello@Air-Juan.fibertel.com.ar>
2024-09-04 23:50:11 -0300

    docs: add mocks for swagger documentation

---
+++ src/swagger/swagger.mocks.ts
@@ -0,0 +1,35 @@
+
+export const exRegisterUser = {
+    lastname: "Gonzales",
+    name: "Maria",
+    email: "admin@gmail.com",
+    password: "admin123"
+}
+
+export const exValidateToken = {
+    code: "1234",
+    email: "admin@gmail.com"
+}
+
+export const exRegisterUserResponse = {
+    "id": "66d67e6160770470da9afe84",
+    "name": "Maria",
+    "lastname": "Gonzales",
+    "email": "admin123@gmail.com"
+  }
+
+  export const exValidateTokenResponde = {
+    "hasError": false,
+    "message": "Validation was succesfully.",
+    "data": true,
+    "status": 200
+  }
+
+  export const exLogin = {
+    email: 'maria@gmail.com',
+    password: 'admin123'
+  }
+
+  export const exLoginResponse = {
+    token : "example"
+  }
