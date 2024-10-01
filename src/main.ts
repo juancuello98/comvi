17079b694a880398b240f1f36f6e6e9ddcba64a5
Juan Cuello <juancuello@Air-Juan.fibertel.com.ar>
2024-10-01 19:55:16 -0300

    fix: remove module.hot innecesary

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,13 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core/nest-factory';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-declare const module: any;
+
 async function bootstrap() {
   const logger = new Logger();
 
   const app = await NestFactory.create(AppModule,{
     logger:['log','error']
   });
-  
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 
   const config = new DocumentBuilder()
     .setTitle('COMVI - API')
