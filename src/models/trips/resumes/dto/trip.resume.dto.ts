d6fd6913f88a1917b58984dc00dbecb0315d3df2
Javiermanzano73261 <manzanojavier@hotmail.com>
2025-07-20 12:44:04 -0300

    refactor

--- src/models/trips/resumes/dto/trip.resume.dto.ts
+++ src/models/trips/resumes/dto/trip.resume.dto.ts
@@ -1,10 +1,4 @@
 export class NewResumeDTO {
-  /**
-   * Lista de IDs de los pasajeros del viaje.
-   */
   passengers: any[];
-  /**
-   * Marca de tiempo de inicio del viaje.
-   */
   startedTimestamp: string;
 }
