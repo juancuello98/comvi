93090cdc30844dde6abf32bdcbc9ff4e656abf54
Javiermanzano73261 <manzanojavier@hotmail.com>
2025-07-20 12:42:47 -0300

    fix: resolve init trip errors

--- src/models/trips/resumes/dto/trip.resume.dto.ts
+++ src/models/trips/resumes/dto/trip.resume.dto.ts
@@ -3,4 +3,8 @@ export class NewResumeDTO {
    * Lista de IDs de los pasajeros del viaje.
    */
   passengers: any[];
+  /**
+   * Marca de tiempo de inicio del viaje.
+   */
+  startedTimestamp: string;
 }
